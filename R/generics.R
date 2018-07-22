e98191f00c79686412a3a3e657e87bcc339dfd2b
Will Lowe <conjugateprior@gmail.com>
2018-07-22 18:20:52 -0400

    Fixed documentation to remove any mention of that naughty quanteda package

--- R/generics.R
+++ R/generics.R
@@ -1,8 +1,8 @@
 #' Summarise Predictions
 #'
 #' This summary function returns a data frame with documents as
-#' rownames and either the most likely author, or posterior
-#' probabilities over all possible authors, for each document.
+#' rownames and for each document, either its the most likely author 
+#' or the probability that it was written by each possible author.
 #' 
 #' If \code{type} is "\code{predicted}",
 #' the columns are "\code{author}", a character vector of predicted author
@@ -15,10 +15,7 @@
 #' If \code{type} is "\code{probs}", each element is the posterior
 #' probability that a document is assigned to each author relative to
 #' the others.  This is constructed from the log posterior
-#' unsing a logistic transformation across rows, a.k.a. 'softmax'.
-#'
-#' Note: if \code{newdata} is a quanteda::corpus then quanteda's \code{docnames} may return generic names.  These are currently 
-#' "text1", "text2", etc. so you may want to change them first.
+#' using a logistic transformation across rows, a.k.a. 'softmax'.
 #' 
 #' @param object Predictions from a stylest_model
 #'               (a \code{\link{stylest_predict}} object)
@@ -65,12 +62,8 @@ summary.stylest_predict <- function(object,
 #' others.  This is constructed from the log posterior by inverse logistic
 #' transformation, a.k.a. 'softmax'.
 #'
-#' Note: if \code{newdata} is a quanteda::corpus then quanteda's \code{docnames}
-#' may return generic names.  These are currently "text1", "text2", etc.
-#'
 #' @param object A fitted stylest_model
-#' @param newdata A character vector of texts, corpus::corpus_frame, or
-#'                quanteda::corpus object
+#' @param newdata A character vector of texts or a \code{\link{corpus_frame}}
 #' @param type Type of prediction "\code{predicted}" (the default),
 #'             "\code{logprobs}" or "\code{probs}". See below for details
 #' @param ... Other arguments (ignored)
